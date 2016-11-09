88d3317d31e1db5f222357f30e4c8afc846155e0
LYleonard <aygxywrp@163.com>
2016-11-09 23:16:27 +0800

    Database operation model realization

--- www/transwarp/db.py
+++ www/transwarp/db.py
@@ -9,33 +9,33 @@ import time, uuid, functools, threading, logging
 # Dict object:
 class Dict(dict):
 	'''
-        Simple dict but support access as x.y style.
-        >> d1 = Dict()
-        >> d1['x'] = 100
-        >> d1.x
-        100
-        >> d1.y = 200
-        >> d1['y']
-        200
-        >> d2 = Dict(a=1, b=2, c='3')
-        >> d2.c
-        '3'
-        >> d2['empty']
-        Traceback (most recent call last):
-            ...
-        KeyError: 'empty'
-        >> d2.empty
-        Traceback (most recent call last):
-            ...
-        AttributeError: 'Dict' object has no attribute 'empty'
-        >> d3 = Dict(('a', 'b', 'c'), (1, 2, 3))
-        >> d3.a
-        1
-        >> d3.b
-        2
-        >> d3.c
-        3
-        '''
+    Simple dict but support access as x.y style.
+    >> d1 = Dict()
+    >> d1['x'] = 100
+    >> d1.x
+    100
+    >> d1.y = 200
+    >> d1['y']
+    200
+    >> d2 = Dict(a=1, b=2, c='3')
+    >> d2.c
+    '3'
+    >> d2['empty']
+    Traceback (most recent call last):
+    ...
+    KeyError: 'empty'
+    >> d2.empty
+    Traceback (most recent call last):
+    ...
+    AttributeError: 'Dict' object has no attribute 'empty'
+    >> d3 = Dict(('a', 'b', 'c'), (1, 2, 3))
+    >> d3.a
+    1
+    >> d3.b
+    2
+    >> d3.c
+    3
+    '''
 	def __init__(self, names=(), values=(), **kw):
 		super(Dict, self).__init__(**kw)
 		for k, v in zip(names, values):
