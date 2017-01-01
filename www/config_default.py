cc8a23896359dca43bcfad1cdb3ec9dd10b9f1b4
LYleonard <aygxywrp@163.com>
2017-01-01 12:10:17 +0800

    add configuration files

---
+++ www/config_default.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'LYleonard'
+
+'''Default configurations'''
+
+configs = {
+    'db':{
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'root',
+        'password': '243015',
+        'database': 'awesome'
+    },
+    'session':{
+        'secret': 'AwEsOmE'
+    }
+}
